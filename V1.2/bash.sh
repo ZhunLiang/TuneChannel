ddd89482810055aa88bbc5dcaf9dc44acc00dcd4
ZhunLiang <594333210@qq.com>
2017-08-29 15:57:16 +0800

    delete folder

--- V1.2/bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/perl
-#INPUT
-#Input order: directory, .gro, .top, detaBin, bulk density, delete ratio
-
-system "cp *.sh *.py *.pl *.mdp $ARGV[0]";
-chdir $ARGV[0];
-system "perl main.pl $ARGV[1] $ARGV[2] $ARGV[3] $ARGV[4] $ARGV[5]"; 
-system "rm -f *.mdp *.sh *.pl *.py *.xvg";
-#system "rm -f *.sh *.pl *.py";
--- V1.2/readme.txt
+++
@@ -1,2 +0,0 @@
-This is for normal channel model
-
